d819e51e65d00eef503ffcf1337e9b5f51340b66
Tenadopt <yury.ignatiev@gmail.com>
2025-04-03 22:00:25 +0200

    feat: add solutions without second

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,14 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number < 0) {
+      reject(new Error('Number is negative.'));
+    } else {
+      resolve(number);
+    }
+  });
 }
 
 /**
@@ -33,8 +39,8 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(source) {
-  return source.then(() => 'success').catch(() => 'fail');
+function getPromiseResult(/* source */) {
+  throw new Error('Not implemented');
 }
 
 /**
